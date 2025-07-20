3bc25d3c541d9c2842b7591412e2ea441bf905f9
sjohnston82 <sjohnston82@users.noreply.github.com>
2025-07-20 09:31:18 -0400

    test2

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -19,7 +19,7 @@ export default function SignupPage() {
     try {
       await signUp.email({ name, email, password });
       router.push("/books");
-      console.log("good");
+      console.log("good1");
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       const message =
